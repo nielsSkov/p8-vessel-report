2fd8953a3c48e46bf637b3441ef14e13df4a8b68
Noelia <noelia.va.3003@gmail.com>
2017-05-21 10:49:49 +0200

    Add outer controllr grapsh with different R

--- figures/Original/Matlab/Outer Controller/PlannerResults.m
+++ figures/Original/Matlab/Outer Controller/PlannerResults.m
@@ -2,16 +2,53 @@
 close all
 clear
 clc
-run ControllersDesign.m
+run Parameters.m
 
 warning off
 
 xbdot = 0.5;
 t = 40;
 
+r = 1;
+R = r;
 sim OuterController.slx
-
-% Analize results
-figure(2)
+figure(1)
 plot(dist.Time,dist.Data)
-FigureLatex('Initial Condition Response of the Outer Controller','Time [s]','Distance [m]',0,0,0,0,0,12,13,1.2)
+hold on
+
+r = 1.5;
+R = r;
+sim OuterController.slx
+figure(1)
+plot(dist.Time,dist.Data)
+hold on
+
+r = 2.5;
+R = r;
+sim OuterController.slx
+figure(1)
+plot(dist.Time,dist.Data)
+hold on
+
+r = 3.5;
+R = r;
+sim OuterController.slx
+figure(1)
+plot(dist.Time,dist.Data)
+hold on
+
+r = 4.5;
+R = r;
+sim OuterController.slx
+figure(1)
+plot(dist.Time,dist.Data)
+hold on
+
+r = 5.5;
+R = r;
+sim OuterController.slx
+figure(1)
+plot(dist.Time,dist.Data)
+hold on
+
+FigureLatex('Initial Condition Response of the Outer Controller','Time [s]','Distance [m]',1,{'R = 1 m','R = 1.5 m','R = 2.5 m','R = 3.5 m','R = 4.5 m','R = 5.5 m'},'NorthEast',0,[0 1],12,13,1.2)
