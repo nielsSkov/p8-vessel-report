94c2e512b8390f2f86c0872a16ca2b0007f1a4ea
Noelia <noelia.va.3003@gmail.com>
2017-05-24 17:06:28 +0200

    Update initial cond graph

--- figures/Original/Matlab/Outer Controller/PlannerResults.m
+++ figures/Original/Matlab/Outer Controller/PlannerResults.m
@@ -9,13 +9,6 @@ warning off
 xbdot = 0.4;
 t = 40;
 
-r = 1;
-R = r;
-sim OuterController.slx
-figure(1)
-plot(dist.Time,dist.Data)
-hold on
-
 r = 1.5;
 R = r;
 sim OuterController.slx
@@ -65,5 +58,12 @@ figure(1)
 plot(dist.Time,dist.Data)
 hold on
 
+r = 5;
+R = r;
+sim OuterController.slx
+figure(1)
+plot(dist.Time,dist.Data)
+hold on
 
-FigureLatex('Initial Condition Response of the Outer Controller','Time [s]','Distance [m]',1,{'R = 1 m','R = 1.5 m','R = 2 m','R = 2.5 m','R = 3 m','R = 3.5 m','R = 4 m','R = 4.5 m'},'NorthEast',0,[0 1],12,13,1.2)
+
+FigureLatex('Initial Condition Response of the Outer Controller','Time [s]','Distance [m]',1,{'R = 1.5 m','R = 2 m','R = 2.5 m','R = 3 m','R = 3.5 m','R = 4 m','R = 4.5 m','R = 5 m'},'NorthEast',0,[0 1],12,13,1.2)
